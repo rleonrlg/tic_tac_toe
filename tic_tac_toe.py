281e0cd7e5e6fda0c9c11761f21105dd92ff20e4
Rodrigo Leon <rleon.rlg@gmail.com>
2022-06-22 15:22:17 -0400

    version 1

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -12,35 +12,36 @@ from functions.free_space import free_space
 from functions.user_move import user_move
 
 
-def mov_pc_hard(board, machine, user):
+def mov_pc_hard(board, pc, user):
     for i in range(9):
         copy = list(board)
         if free_space(copy, i):
-            copy[i] = machine
-            if winner(copy, machine):
-                return i
-
+            copy[i] = pc
+            if winner(copy, pc):
+                return i                           
+                          
     for i in range(9):
         copy = list(board)
         if free_space(copy, i):
             copy[i] = user
             if winner(copy, user):
                 return i
-    if pc == "x":
+    if pc == "X":
         if board[4] == " ":
             return 4
-        empty_courner = []
-        for i in [0, 2, 6, 8]:
-            if free_space(board, i):
-                empty_courner.append(i)
-        empty_spaces = []
-        for i in [1, 3, 5 ,7]:
-            if free_space(board, i):
-                empty_spaces.append(i)
-        if len(empty_courner) <  0:
-            return random.choice(empty_courner)
         else:
-            return random.choice(empty_spaces)
+            empty_courner = []
+            for i in [0, 2, 6, 8]:
+                if free_space(board, i):
+                    empty_courner.append(i)
+            empty_spaces = []
+            for i in [1, 3, 5 ,7]:
+                if free_space(board, i):
+                    empty_spaces.append(i)
+                    if len(empty_courner) <= 0:
+                        return random.choice(empty_courner)
+                    else:
+                        return random.choice(empty_spaces)
     if pc == "O":
         num = 0
         for i in range(9):
